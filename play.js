3dc2cc411b2a8aeb33682596757a82d953f2275f
Mitch-Kenward <mkenward29@gmail.com>
2021-04-09 22:24:17 +0000

    server message

--- play.js
+++ play.js
@@ -1,17 +1,23 @@
+
 const net = require('net');
 
 //Establishes connection with game server
 
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '135.23.222.131',
+    host: '10.0.2.15',
     port: 50541
   });
+  
   // interpret incoming data as text
   conn.setEncoding('utf8'); 
-
+  conn.on('connect', function() {
+    console.log('We are connected');
+    conn.write("Name: mrk");
+  }) 
   return conn;
 }
 
 console.log('Connecting ...');
 connect();
+
