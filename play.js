0feff97e409588fcf5f945fc85aef077fb853538
Mitch-Kenward <mkenward29@gmail.com>
2021-04-12 02:35:07 +0000

    move up

--- play.js
+++ play.js
@@ -4,20 +4,28 @@ const net = require('net');
 //Establishes connection with game server
 
 const connect = function() {
-  const conn = net.createConnection({ 
+  const conn = net.createConnection({
     host: '10.0.2.15',
     port: 50541
   });
   
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+  conn.setEncoding('utf8');
+
   conn.on('connect', function() {
     console.log('We are connected');
-    conn.write("Name: mrk");
-  }) 
+    //conn.write("Name: mrk");
+
+    console.log('Connecting ...');
+    setInterval(function() {
+      conn.write("Move: up");
+    }, 5000);
+
+
+   
+    
+  });
+
   return conn;
-}
-
-console.log('Connecting ...');
+};
 connect();
-
