c30dbb03f8186df271a2971a207564ecba79835a
Mitch-Kenward <mkenward29@gmail.com>
2021-04-12 02:53:44 +0000

    user input

--- play.js
+++ play.js
@@ -1,8 +1,23 @@
 
 const net = require('net');
-
 //Establishes connection with game server
+/**
+ * Setup User Interface
+ * Specifically, so that we can handle user input via stdin
+ */
+const setupInput = function(handleUserInput) {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
 
+  stdin.on('data', (key) => {
+  
+    handleUserInput(key);
+  });
+
+  stdin.resume();
+  return stdin;
+};
 const connect = function() {
   const conn = net.createConnection({
     host: '10.0.2.15',
@@ -17,15 +32,21 @@ const connect = function() {
     //conn.write("Name: mrk");
 
     console.log('Connecting ...');
-    setInterval(function() {
-      conn.write("Move: up");
-    }, 5000);
+    // setInterval(function() {
+    // conn.write("Move: up");
+    // }, 5000);
+    setupInput(handleUserInput);
 
-
-   
-    
   });
 
   return conn;
 };
 connect();
+
+const handleUserInput = function(key) {
+  if (key === '\u0003') {
+    process.exit();
+  }
+};
+
+
